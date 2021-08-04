2bf2151aa26669131cb4e0b08caffa9e31468d35
jasmith188 <jasmith.188@gmail.com>
2021-08-04 14:02:06 -0400

    initial commit

---
+++ routes/quote.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Quote = require("../models/Quote");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Quote.find()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+
+router.post("/", (req, res) => {
+  const quote = new Quote({
+    quote: req.body.quote,
+    author: req.body.author,
+  });
+  quote
+    .save()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+router.delete("/:quoteID", (req, res) => {
+  Quote.deleteOne({ _id: req.params.quoteID })
+    .then(() => res.status(200).json({ message: "Quote deleted" }))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+
+router.patch("/:quoteID", (req, res) => {
+  Quote.updateOne(
+    { _id: req.params.quoteID },
+    {
+      $set: {
+        quote: req.body.quote,
+        author: req.body.author,
+      },
+    }
+  )
+    .then((data) => res.status(200).json({ message: "Quote updated" }))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+
+module.exports = router;
